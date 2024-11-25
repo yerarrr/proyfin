87bc4f9b9be15871eca6bc9cc63e223178e23cfb
yerarrr <pawelyerarcarbajalluna@gmail.com>
2024-11-25 00:30:36 -0500

    5

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,7 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
+import { appRoutes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
@@ -9,5 +9,5 @@ import { getMessaging, provideMessaging } from '@angular/fire/messaging';
 import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"proyfin-50bf3","appId":"1:733333223618:web:6ecbec040df546bf6c235b","storageBucket":"proyfin-50bf3.firebasestorage.app","apiKey":"AIzaSyBKGCeSi8thK0x1XWhEnnN-x4GBGa1B1ls","authDomain":"proyfin-50bf3.firebaseapp.com","messagingSenderId":"733333223618","measurementId":"G-SJP8CH2VW2"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(appRoutes), provideFirebaseApp(() => initializeApp({"projectId":"proyfin-50bf3","appId":"1:733333223618:web:6ecbec040df546bf6c235b","storageBucket":"proyfin-50bf3.firebasestorage.app","apiKey":"AIzaSyBKGCeSi8thK0x1XWhEnnN-x4GBGa1B1ls","authDomain":"proyfin-50bf3.firebaseapp.com","messagingSenderId":"733333223618","measurementId":"G-SJP8CH2VW2"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
-export const routes: Routes = [];
+export const appRoutes: Routes = [
+  // Ruta predeterminada que redirige a "login"
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+
+  // Ruta para el componente de inicio de sesión
+  { path: 'login', component: LoginComponent },
+
+  // Ruta para el componente de registro
+  { path: 'register', component: RegisterComponent },
+
+  // Ruta comodín para páginas no encontradas
+  { path: '**', redirectTo: 'login' },
+];
