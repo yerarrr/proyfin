d8d03c24a282637b503bd26005d157cf9df2852a
yerarrr <pawelyerarcarbajalluna@gmail.com>
2024-12-08 06:46:41 -0500

    18

---
+++ src/app/task/ui/table/table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './table.component.html',
+  styles: ``
+})
+export class TableComponent {
+
+}
